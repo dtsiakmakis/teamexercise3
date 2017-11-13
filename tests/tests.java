e1887ffa517801d68efe97987a288a755c09c20c
Melanie <mldworak@wpi.edu>
2017-11-12 20:00:01 -0500

    added tests

--- tests/tests.java
+++ tests/tests.java
@@ -1,6 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Melanie on 11/12/2017.
  */
 public class tests {
+    Rectangle easy = new Rectangle(10, 10);
 
+
+    @Test
+    public void perimeter(){
+        assertEquals(easy.getPerimeter(),40.0, 0.0);
+    }
 }
